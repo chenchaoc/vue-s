1a4b0335396c5d5188570a2afde2ae25b7ce06c2
chenchao <chenchao3.sh@superjia.com>
2018-08-23 09:44:40 +0800

    fix

--- build/config/devPlugins.js
+++ build/config/devPlugins.js
@@ -3,10 +3,12 @@
 * @Date: 2018-08-21 16:07:22
 * @Email: chenchao3@sh.superjia.com
  * @Last Modified by: chenchao
- * @Last Modified time: 2018-08-22 15:42:02
+ * @Last Modified time: 2018-08-22 21:22:12
 */
 import webpack from 'webpack'
+import { BundleAnalyzerPlugin }  from 'webpack-bundle-analyzer' //可视化查看打包后的文件
 
 export default [
-  new webpack.HotModuleReplacementPlugin()
+  new webpack.HotModuleReplacementPlugin(),
+  //new BundleAnalyzerPlugin(), //可视化查看打包后的文件
 ]
--- src/api.js
+++ src/api.js
@@ -1,20 +1,20 @@
 import store from './store/store'
 
 const apiMap = {
-  homeList: 'homeList',
+  homeList: 'homeList.action',
 }
 //例子  API('homeList', { a: 1 }, { headers: { OS: 'WECHAT' } })
 export default function(name, data = {}, options = {}) {
-  const { showLoading = true, hideToast } = options
-  showLoading && store.dispatch('addLoading')
-  return common.ajax(`${apiMap[name]}.action`, data, options).then((res) => {
-    showLoading && store.dispatch('stopLoading')
+  //const { showLoading = true, hideToast } = options
+  //showLoading && store.dispatch('addLoading')
+  return common.ajax(apiMap[name], data, options).then((res) => {
+    //showLoading && store.dispatch('stopLoading')
     if (res.errorCode == 0) {
       return res
     }
     return Promise.reject(res)
   }).catch((error) => {
-    showLoading && store.dispatch('stopLoading')
+    //showLoading && store.dispatch('stopLoading')
     //!hideToast && toast(error.message || '网络异常，请稍后再试')
     return Promise.reject(error)
   })
